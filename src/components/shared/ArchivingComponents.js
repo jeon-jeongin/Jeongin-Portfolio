765d88d6ff21379c7be6f40e6a609d647786fe57
jeon-jeongin <wjddls1702@naver.com>
2022-04-27 22:27:26 +0900

    ADD archiving page

---
+++ src/components/shared/ArchivingComponents.js
@@ -0,0 +1,25 @@
+import { Box, styled } from "@material-ui/core";
+import styledComponents from "styled-components";
+
+export const ArchivingContent = styled(Box)({
+    padding: '0.5rem 1rem'
+})
+
+export const ArchivingTitle = styledComponents.span`
+    line-height: 50px;
+`
+export const SubTitle = styledComponents.div`
+    color: #1696D8;
+    margin-bottom: 15px;
+    &:hover{
+        text-decoration: underline;
+    }
+`
+
+export const FontBold = styledComponents.span`
+    font-weight: 600;
+`
+
+export const Content = styledComponents.li`
+    margin-top: 15px;
+`
