84cbbe813346ebb6a5d08b3438b7cf77eb2d08df
jeon-jeongin <wjddls1702@naver.com>
2022-05-02 03:07:12 +0900

    EDIT project portfolio image

--- src/components/Project/Portfolio.js
+++ src/components/Project/Portfolio.js
@@ -1,10 +1,11 @@
 import { Grid } from "@material-ui/core";
 import Slider from "react-slick/lib/slider";
 import { ContentDetail, Contents, Images, ProjectDay, ProjectsContaienr, ProjectSub, ProjectTitile, Url } from "../shared/ProjectComponents";
-import main from '../../images/ourlife/main.png';
-import login from '../../images/ourlife/login.png';
-import signup from '../../images/ourlife/signup.png';
-import mypage from '../../images/ourlife/mypage.png';
+import home from '../../images/portfolio/home.png';
+import aboutme from '../../images/portfolio/aboutme.png';
+import archiving from '../../images/portfolio/archiving.png';
+import skills from '../../images/portfolio/skills.png';
+import project from '../../images/portfolio/project.png';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { UnderLine } from "../shared/Shared";
@@ -29,16 +30,19 @@ function Portfolio() {
                 <Grid item md={7} sm={12} xs={12} style={{ marginBottom: '1.5rem' }}>
                     <Slider {...settings}>
                         <div>
-                            <Images src={main} alt="메인 홈" />
+                            <Images src={home} alt="메인 홈" />
                         </div>
                         <div>
-                            <Images src={login} alt="로그인 페이지" />
+                            <Images src={aboutme} alt="나의 관한 내용" />
                         </div>
                         <div>
-                            <Images src={signup} alt="회원 가입 페이지" />
+                            <Images src={archiving} alt="백업" />
                         </div>
                         <div>
-                            <Images src={mypage} alt="마이 페이지" />
+                            <Images src={skills} alt="나의 스킬" />
+                        </div>
+                        <div>
+                            <Images src={project} alt="프로젝트" />
                         </div>
                     </Slider>
                 </Grid>
@@ -55,6 +59,7 @@ function Portfolio() {
                     <ProjectSub variant="subtitle1">사용 기술</ProjectSub>
                     <Contents>
                         <ContentDetail>Frontend : React, CSS3</ContentDetail>
+                        <ContentDetail>Deployment : Heroku</ContentDetail>
                     </Contents>
                     <ProjectSub variant="subtitle1">GitHub</ProjectSub>
                     <a href="https://github.com/jeon-jeongin/Jeongin-Portfolio" target='_blank' rel="noreferrer">
